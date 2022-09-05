99f7ba1ac68efbee5511a219cdc2fee7b0ec6a84
Sami Rherbi <sami0771@edu.zealand.dk>
2022-09-05 12:42:36 +0200

    Test prop & propfull

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    class Demo
+    {
+        public int MyProperty { get; set; }
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+    }
+}
